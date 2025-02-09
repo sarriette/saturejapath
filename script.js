cbaf2d8e4d45e574effe5fef1b9507d73b019751
sarriette <sarriettedesmontagnes@protonmail.com>
2025-02-09 18:00:02 +0100

    resolve conflict

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
 let currentImageIndex = 0;
 const images = [
-    "images/posters/casse-tete.png",
-    "images/posters/violence.png",
+    "images/posters/casse-tete.jpg",
+    "images/posters/violence.jpg",
 ];
 
 function openGallery(index) {
@@ -25,31 +24,3 @@ function navigate(direction) {
 function updateImage() {
     document.getElementById("largeImage").src = images[currentImageIndex];
 }
-=======
-let currentImageIndex = 0;
-const images = [
-    "images/posters/casse-tete.png",
-    "images/posters/violence.png",
-];
-
-function openGallery(index) {
-    currentImageIndex = index;
-    document.getElementById("galleryModal").style.display = "flex";
-    updateImage();
-}
-
-function closeGallery() {
-    document.getElementById("galleryModal").style.display = "none";
-}
-
-function navigate(direction) {
-    currentImageIndex += direction;
-    if (currentImageIndex < 0) currentImageIndex = images.length - 1;
-    if (currentImageIndex >= images.length) currentImageIndex = 0;
-    updateImage();
-}
-
-function updateImage() {
-    document.getElementById("largeImage").src = images[currentImageIndex];
-}
->>>>>>> c07e554551e2be61030ae75c9964d19799769666
