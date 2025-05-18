2abb372064cd04ff53dea75b955607f2395f777c
sarriette <sarriettedesmontagnes@protonmail.com>
2025-05-18 19:09:26 +0200

    reorganized menu

---
+++ menu.js
@@ -0,0 +1,30 @@
+fetch('menu.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('menu-container').innerHTML = data;
+
+    // Fonction appelée par le bouton "Random project"
+    window.goToRandomProject = function () {
+      const projectPages = [
+        'cats2.html',
+        'charlouse.html',
+        'ciboulette.html',
+        'coco-elements.html',
+        'echalote.html',
+        'family-pictures.html',
+        'harvey.html',
+        'kakis.html',
+        'massilia.html',
+        'petiteboule.html',
+        'petitpois.html',
+        'portraits.html',
+        'posters.html',
+        'randomcats.html',
+        'spatule.html',
+        'titouan-s-room.html',
+        'tulba.html',
+      ];
+      const randomIndex = Math.floor(Math.random() * projectPages.length);
+      window.location.href = projectPages[randomIndex];
+    };
+  });
